46d43725e3a755039ac35e1b606b556b094e0371
Vincenzo Marino <vincenzomarino2098@gmail.com>
2019-01-31 17:18:59 +0100

    check out

--- app/src/main/java/com/vincenzo/example/depeat/ui/activities/MainActivity.java
+++ app/src/main/java/com/vincenzo/example/depeat/ui/activities/MainActivity.java
@@ -1,7 +1,11 @@
 package com.vincenzo.example.depeat.ui.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.vincenzo.example.depeat.R;
 
@@ -11,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.login_menu){
+            startActivity(new Intent(this,LoginActivity.class));
+            return true;
+        }else if(item.getItemId() == R.id.checkout_menu){
+            startActivity(new Intent(this, CheckOutActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
